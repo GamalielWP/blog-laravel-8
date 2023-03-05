2290299ab6b9bdec1f8d9ec92d5c6bf5d50cc594
GamalielWP <gamalielwp@gmail.com>
2023-03-05 11:02:36 +0700

    Add Seeder

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,11 +4,20 @@
 <h1 class="mb-5">Halaman Blog Posts</h1>
 
     @foreach ($posts as $post)
-        <article class="mb-5">
-            <a href="/posts/{{ $post->slug }}">
-                <h2>{{ $post->title }}</h2>
-            </a>
+        <article class="mb-5 border-bottom pb-4">
+            <h2>
+                <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+            </h2>
+
+            <p>By. <a class="text-decoration-none" href="">{{ $post->user->name }}</a> in
+                <a class="text-decoration-none" href="/categories/{{ $post->category->slug }}">
+                    {{ $post->category->name }}
+                </a>
+            </p>
+
             <p>{{ $post->excerpt }}</p>
+
+            <a class="text-decoration-none" href="/posts/{{ $post->slug }}">Read more..</a>
         </article>
     @endforeach
 @endsection
