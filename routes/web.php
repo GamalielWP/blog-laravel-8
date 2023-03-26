d78949faca9b717164dd93560231353099e13162
GamalielWP <gamalielwp@gmail.com>
2023-03-26 17:35:53 +0700

    Add view login & registration

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use App\Models\Category;
-use App\Models\Post;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -65,3 +65,7 @@ Route::get('categories', function(){
 // });
 
 Route::get('/getPhotos/{categorName}', [PostController::class, 'getPhotos']);
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
